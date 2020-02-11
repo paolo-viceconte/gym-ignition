a72da8ee165b3de378f96210b4a1ac5ca8b29082
Paolo Viceconte <paolomariaviceconte@gmail.com>
2020-02-11 17:47:51 +0100

    temporary modifications

--- examples/python/launch_panda.py
+++ examples/python/launch_panda.py
@@ -5,7 +5,7 @@
 # GNU Lesser General Public License v2.1 or any later version.
 
 import gym
-import time
+import numpy as np
 
 # Set gym verbosity
 gym.logger.set_level(gym.logger.INFO)
@@ -23,74 +23,50 @@ env.render('human')
 
 # # Initialize the seed
 # env.seed(42)
-#
-# for epoch in range(10):
-#     # Reset the environment
-#     observation = env.reset()
-#
-#     # Initialize returned values
-#     done = False
-#     totalReward = 0
-#
-#     while not done:
-#         # Execute a random action
-#         action = env.action_space.sample()
-#         observation, reward, done, _ = env.step(action)
-#
-#         # Render the environment
-#         # It is not required to call this in the loop
-#         # env.render('human')
-#
-#         # Accumulate the reward
-#         totalReward += reward
-#
-#         # Print the observation
-#         msg = ""
-#         for value in observation:
-#             msg += "\t%.6f" % value
-#         logger.debug(msg)
-#
-#     logger.info(f"Total reward for episode #{epoch}: {totalReward}")
-
 
 for epoch in range(10):
-    print(epoch)
+
+    #print("NEW_EPOCH")
+
     # Reset the environment
     observation = env.reset()
+
+    #print("reset done")
+    #print(env.action_space)
+    #print(env.observation_space)
 
     # Initialize returned values
     done = False
     totalReward = 0
+    step_counter=0
+    rollout_termination_counter=0
 
     while not done:
         # Execute a random action
         action = env.action_space.sample()
 
-        observation, reward, done, _ = env.step(action)
+        #print(step_counter)
+        step_counter+=1
+
+        observation, reward, done, _ = env.step(action)  # it gets stuck here sometimes
 
         # Render the environment
         # It is not required to call this in the loop
-        env.render('human')
+        # env.render('human')
 
         # Accumulate the reward
         totalReward += reward
-
-        # print("---------------ACTION-------------------")
-        # print(action)
-        # print("---------------OBSERVATION-------------------")
-        # print(observation)
-        # print("---------------REWARD-------------------")
-        # print(reward)
-        # print("---------------DONE-------------------")
-        # print(done)
-        # print("---------------TOTAL REWARD-------------------")
-        # print(totalReward)
-        # time.sleep(1)
 
         # Print the observation
         msg = ""
         for value in observation:
             msg += "\t%.6f" % value
-        logger.debug(msg)
+        # logger.debug(msg)
+
+        rollout_termination_counter+=1
+        if (rollout_termination_counter>1000):
+            done=True
+
+#     logger.info(f"Total reward for episode #{epoch}: {totalReward}")
 
 env.close()
